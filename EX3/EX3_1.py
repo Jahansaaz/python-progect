93a1e204e0301675979facfa20ab51f7ea5fa2fa
Jahansaaz <116023721+Jahansaaz@users.noreply.github.com>
2022-11-22 19:05:19 -0800

    Add files via upload

---
+++ EX3/EX3_1.py
@@ -0,0 +1,18 @@
+def EX3_1(x):
+     '''
+     this function will repetedly evalut the operation,until reach 1
+     example:
+     EX3_1(2)-> 1
+     '''
+     lst=[]
+     while x>=1:
+         if x%2==0:
+             lst.append(x/2)
+
+         else x%2!=0:
+             lst.append(x*3+1)
+
+     while x==1
+        print(x)
+
+     print(len(lst))
